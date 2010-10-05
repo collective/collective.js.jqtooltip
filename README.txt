deb8f9da85eaa8e7170bb35461172602c2903a26
Timon Tschanz <timon.tschanz@4teamwork.ch>
2010-10-05 17:28:56 +0200

    inital commit

---
+++ README.txt
@@ -0,0 +1,4 @@
+Introduction
+============
+
+This Plugin allows you to use Jquery.tooltip where ever you want without inserting the .js file every time. 
---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='collective.js.jqtooltip',
+      version=version,
+      description="This Package allows you to use jquery.tooltip",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python"
+        ],
+      keywords='',
+      author='Timon Tschanz',
+      author_email='t.tschanz@4teamwork.ch',
+      url='http://bassistance.de/jquery-plugins/jquery-plugin-tooltip/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['collective', 'collective.js'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['setuptools',
+                        'ftw.utilities',
+                        'archetypes.schemaextender',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
