f1fb1fd6fb12fe970d5543c87a8fdcae8b2387e5
Jonas Baumann <jone@jone.ch>
2011-11-30 15:12:20 +0100

    Update package metadata.

--- setup.py
+++ setup.py
@@ -6,26 +6,37 @@ maintainer = 'Timon Tschanz'
 
 setup(name='collective.js.jqtooltip',
       version=version,
-      description="This Package allows you to use jquery.tooltip",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "History.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      description='This package allows you to use jquery.tooltip in plone.',
+      long_description=open('README.txt').read() + '\n' + \
+          open(os.path.join('docs', 'History.txt')).read(),
+
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Programming Language :: Python"
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.0',
+        'Framework :: Plone :: 4.1',
+        'Programming Language :: Python'
         ],
+
       keywords='',
       author='Timon Tschanz',
       author_email='t.tschanz@4teamwork.ch',
-      url='http://bassistance.de/jquery-plugins/jquery-plugin-tooltip/',
-      license='GPL',
+      url='https://github.com/collective/collective.js.jqtooltip',
+      license='GPL2',
+
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['collective', 'collective.js'],
       include_package_data=True,
       zip_safe=False,
+
       install_requires=[
-      'setuptools',
-      ],
-      entry_points="""
+        'setuptools',
+        ],
+
+      entry_points='''
       # -*- Entry points: -*-
-      """,
+      [z3c.autoinclude.plugin]
+      target = plone
+      ''',
       )
