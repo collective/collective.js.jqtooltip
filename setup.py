f199c2b97657545617cafe9ec19b171f98721039
Jonas Baumann <jone@jone.ch>
2011-11-30 15:08:17 +0100

    Move version from version.txt to setup.py.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = open('collective/js/jqtooltip/version.txt').read().strip()
+version = '1.1dev'
 maintainer = 'Timon Tschanz'
 
 setup(name='collective.js.jqtooltip',
